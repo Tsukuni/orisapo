310b772d3cc90f22db499fab21c53188186dc45b
Tsukuni <tamkchi.fugu@gmail.com>
2019-02-28 22:02:17 +0900

    動画挿入

--- js/index.js
+++ js/index.js
@@ -1,15 +1,2 @@
 $(function() {
-  console.log("a")
-  const activeClass = 'active';
-
-  function hamburgBtn() {
-    $('.btn_hamburger').on('click', function(event) {
-      const button = $(this);
-      event.preventDefault();
-
-      button.find('a').toggleClass(activeClass);
-    });
-  };
-  hamburgBtn();
 });
-
